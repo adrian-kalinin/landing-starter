036058c7e338bad6977054b1b90fb5e24feb52c3
Adrian Kalinin <adriankalinin@protonmail.com>
2025-01-14 20:42:30 +0200

    Install eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: {
+    overrides: {
+      'vue/max-attributes-per-line': ['error', { singleline: { max: 3 }, multiline: { max: 1 } }],
+      'vue/attributes-order': 'error',
+    },
+  },
+  formatters: true,
+})
