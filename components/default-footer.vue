40d33f246a27b5a124c28fbb01c2966bf530b7e8
Adrian Kalinin <adriankalinin@protonmail.com>
2025-01-15 22:01:51 +0200

    Add default footer

---
+++ components/default-footer.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+interface Link {
+  label: string
+  to: string
+}
+
+const exploreLinks: Link[] = [
+  { label: 'Features', to: '#' },
+  { label: 'Pricing', to: '#' },
+  { label: 'FAQ', to: '#' },
+]
+
+const legalLinks: Link[] = [
+  { label: 'Terms of use', to: '#' },
+  { label: 'Privacy policy', to: '#' },
+  { label: 'Cookie policy', to: '#' },
+]
+
+const companyLinks: Link[] = [
+  { label: 'About us', to: '#' },
+  { label: 'Contact', to: '#' },
+]
+</script>
+
+<template>
+  <footer class="footer bg-neutral text-neutral-content p-10">
+    <nav>
+      <h6 class="footer-title">
+        Explore
+      </h6>
+      <NuxtLink
+        v-for="link in exploreLinks"
+        :key="link.label"
+        :to="link.to"
+        class="link link-hover"
+      >
+        {{ link.label }}
+      </NuxtLink>
+    </nav>
+    <nav>
+      <h6 class="footer-title">
+        Legal
+      </h6>
+      <NuxtLink
+        v-for="link in legalLinks"
+        :key="link.label"
+        :to="link.to"
+        class="link link-hover"
+      >
+        {{ link.label }}
+      </NuxtLink>
+    </nav>
+    <nav>
+      <h6 class="footer-title">
+        Company
+      </h6>
+      <NuxtLink
+        v-for="link in companyLinks"
+        :key="link.label"
+        :to="link.to"
+        class="link link-hover"
+      >
+        {{ link.label }}
+      </NuxtLink>
+    </nav>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,6 @@
   <div>
     <DefaultHeader />
     <slot />
+    <DefaultFooter />
   </div>
 </template>
