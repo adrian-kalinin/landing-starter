39d9dc1723e43e8e86d1c0bc0466101ebddf6f28
Adrian Kalinin <adriankalinin@protonmail.com>
2025-01-15 22:42:27 +0200

    Add hero section

---
+++ components/section-hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hero bg-base-200 h-[calc(100vh-4rem)]">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <img
+        src="https://placehold.co/600x400?text=..."
+        class="w-xl max-w-full rounded-lg shadow-sm"
+      >
+      <div>
+        <h1 class="text-5xl font-bold">
+          Lorem Ipsum
+        </h1>
+        <p class="py-6">
+          Aliquam mauris velit, lobortis sed dignissim bibendum, semper at orci. Cras bibendum, nisl a tempor tincidunt, nibh eros aliquam massa, eu tempus risus turpis vitae ex.
+        </p>
+        <NuxtLink to="#" class="btn btn-primary">
+          Get Started
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
