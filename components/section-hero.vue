096e2d6a6362d3965128208808521c5d8b844012
Adrian Kalinin <adriankalinin@protonmail.com>
2025-01-24 22:48:36 +0200

    Update design of hero section

--- components/section-hero.vue
+++ components/section-hero.vue
@@ -1,21 +1,24 @@
 <template>
-  <section id="hero" class="hero h-[calc(100vh-4rem)]">
-    <div class="hero-content flex-col lg:flex-row-reverse">
-      <img src="https://placehold.co/600x400?text=..." class="w-xl max-w-full rounded-lg shadow-sm">
-      <div class="px-6">
-        <h1 class="text-5xl font-bold">
-          Lorem Ipsum
+  <section id="hero" class="hero min-h-[calc(100vh-4rem)] container mx-auto">
+    <div class="hero-content flex-col lg:flex-row-reverse gap-8 py-16">
+      <div class="lg:w-1/2">
+        <img src="https://placehold.co/600x400?text=Hello" class="w-full rounded-lg shadow-xl">
+      </div>
+      <div class="lg:w-1/2">
+        <h1 class="text-5xl font-bold leading-tight">
+          Revolutionize your business with our <span class="text-primary">dynamic</span> solutions.
         </h1>
-        <p class="py-6">
-          Aliquam mauris velit, lobortis sed dignissim bibendum, semper at orci. Cras bibendum, nisl a tempor tincidunt,
-          nibh eros aliquam massa, eu tempus risus turpis vitae ex.
+        <p class="py-6 text-base-content/70 text-lg">
+          Elevate your business to new heights with our cutting-edge solutions. Seamlessly integrate powerful features, flexible pricing, and dedicated support.
         </p>
-        <NuxtLink to="#" class="btn btn-primary">
-          Get Started
-        </NuxtLink>
-        <NuxtLink to="#features" class="btn btn-ghost ml-4">
-          Learn More
-        </NuxtLink>
+        <div class="flex gap-4">
+          <NuxtLink to="#" class="btn btn-primary">
+            Get Started
+          </NuxtLink>
+          <NuxtLink to="#features" class="btn btn-ghost">
+            Learn More
+          </NuxtLink>
+        </div>
       </div>
     </div>
   </section>
